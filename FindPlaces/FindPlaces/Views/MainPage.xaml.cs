6001b9b61014339bce0ba2ad1bf23b1333e36820
RIYAZ <v-mriyaz@microsoft.com>
2017-06-13 19:47:28 +0530

    OnAppearring implementation

--- FindPlaces/FindPlaces/Views/MainPage.xaml.cs
+++ FindPlaces/FindPlaces/Views/MainPage.xaml.cs
@@ -1,12 +1,24 @@
-﻿using Xamarin.Forms;
+﻿using FindPlaces.ViewModels;
+using Xamarin.Forms;
 
 namespace FindPlaces.Views
 {
     public partial class MainPage : ContentPage
     {
+        internal MainPageViewModel ViewModel { get { return BindingContext as MainPageViewModel; } }
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void ListView_ItemAppearing(object sender, ItemVisibilityEventArgs e)
+        {
+            if (ViewModel.IsBusy && ViewModel.Places.Count == 0)
+                return;
+            if (e.Item.Equals(ViewModel.Places[ViewModel.Places.Count - 1]))
+            {
+                //Load Items
+            }
         }
     }
 }
